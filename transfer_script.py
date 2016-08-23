7e23126bc8c154f17cdf171a9f3578b420f71993
frankserrino <frank@serrino.com>
2016-08-23 09:00:09 -0500

    Update transfer_script.py

--- transfer_script.py
+++ transfer_script.py
@@ -1,11 +1,9 @@
-# -*- coding: utf-8 -*-
+
 """
 Created on Mon Aug 22 19:51:38 2016
 
 @author: fs, copied from another source and modified
 """
-
-
 
 import os
 from path import path
@@ -16,18 +14,22 @@ COPY_DIRECTORY = '/Users/fs/BBId/testdir'  # Destination directory
 d = path(DIRECTORY)
 copy_directory = path(COPY_DIRECTORY)
 
-
-def transfer():
+def transform():
     print "Copying Files from %s to %s" % (d, copy_directory)
     file_count = 0
     for i in d.walk():
         if i.isfile() and i.endswith('jpg'):
             file_count += 1
             print "Copying %s" % i
+
+# ***  between this comment and the one below is the place we place our transformation code
+
             i.copy(copy_directory)
 
-    print 'Transferred %s files' % file_count
+# ***
+
+    print 'Transformed %s files' % file_count
 
 
 if __name__ == "__main__":
-    transfer()
+    transform()
